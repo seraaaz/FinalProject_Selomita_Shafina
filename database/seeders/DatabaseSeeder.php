712015ca6080bc6c1049614c0ef38c5e0c4ad0c4
seraaaz <selomita.zhafiirah@gmail.com>
2023-10-18 21:43:42 +0700

    adding seeder for reservation and test factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ReservationSeeder::class,
+            UserSeeder::class,
+        ]);
+            // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
---
+++ database/seeders/ReservationSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\reservation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReservationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        reservation::create([
+            'name' => 'John Doe',
+            'phone_number' => '123456789',
+            'email' => 'john@gmail.com',
+            'date' => '2021-01-01',
+            'time' => '12:00',
+            'guests' => '2',
+            'message' => 'Hello, I would like to reserve a table for two.',
+        ]);
+        
+    }
+}
